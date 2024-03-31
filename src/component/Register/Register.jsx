ccb4969d93f3bf4634a7d44e9cffc3341c683a6f
shahinexy <shahinexy@gmail.com>
2024-03-31 16:16:47 +0600

    password show hide done

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -3,77 +3,96 @@ import auth from "../../firebase.config";
 import { useState } from "react";
 
 const Register = () => {
-  const [registerError, setRegisterError] = useState('')
-  const [success, setSuccess] = useState('')
+  const [registerError, setRegisterError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [showPass, setShowPass] = useState(false)
 
-  const handleLogin = e=>{
-    e.preventDefault()
+  const handleLogin = (e) => {
+    e.preventDefault();
     const email = e.target.email.value;
     const passowrd = e.target.password.value;
 
-    if(passowrd.length < 6){
-      setRegisterError('Password should be at least 6 characters or longer')
-      return
-    }
-    else if(!/[A-Z]/.test(passowrd)){
-      setRegisterError('Passowrd should be contain at least one Uppercase character')
+    if (passowrd.length < 6) {
+      setRegisterError("Password should be at least 6 characters or longer");
+      return;
+    } else if (!/[A-Z]/.test(passowrd)) {
+      setRegisterError(
+        "Passowrd should be contain at least one Uppercase character"
+      );
       return;
     }
 
-    setRegisterError('')
-    setSuccess('')
+    setRegisterError("");
+    setSuccess("");
 
     createUserWithEmailAndPassword(auth, email, passowrd)
-    .then(result => {
+      .then((result) => {
         console.log(result.user);
-        setSuccess('Successfully Login')
-    })
-    .catch(error =>{
+        setSuccess("Successfully Login");
+      })
+      .catch((error) => {
         console.log(error.message);
-        setRegisterError(error.message)
-    })
-}
+        setRegisterError(error.message);
+      });
+  };
 
-    return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-5">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type={showPass ? 'text' : 'password'}
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <span onClick={()=> setShowPass(!showPass)} className="border p-1 text-center cursor-pointer">
+                  {showPass ? 'Hide': 'Show'}
+                </span>
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            {registerError && <p className="text-red-500">{registerError}</p>}
+            {success && <p className="text-green-500">{success}</p>}
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-5">
-      <form onSubmit={handleLogin} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-      {
-        registerError && <p className="text-red-500">{registerError}</p>
-      }
-      {
-        success && <p className="text-green-500">{success}</p>
-      }
-    </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Register;
