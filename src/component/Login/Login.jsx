9b00d470c5c68ddc4ed56ca8b1a66f097a4b3ad4
shahinexy <shahinexy@gmail.com>
2024-03-31 15:56:42 +0600

    register login valideted

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,15 +1,32 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase.config";
+
 
 const Login = () => {
+    const handleLogin = e=>{
+        e.preventDefault()
+        const email = e.target.email.value;
+        const passowrd = e.target.password.value;
+        console.log(email, passowrd);
+        createUserWithEmailAndPassword(auth, email, passowrd)
+        .then(result => {
+            console.log(result.user);
+        })
+        .catch(error =>{
+            console.log(error.message);
+        })
+    }
+
     return (
         <div>
             <div className="w-1/2 mx-auto bg-gray-300 p-10 my-5">
             <h2 className='text-3xl my-5'>Login Now</h2>
-            <form className="space-y-5">
-                <input type="text" name="name" id="" placeholder="Enter Name"  className="p-3"/>
+            <form onSubmit={handleLogin} className="space-y-5 ">
+                <input type="email" name="email"  placeholder="Enter Email"  className="p-3"/>
                 <br />
-                <input type="email" name="email" id="" placeholder="Enter Email"  className="p-3"/>
+                <input type="password" name="password"placeholder="Enter Password"  className="p-3"/>
                 <br />
-                <input type="passowrd" name="password" id="" placeholder="Enter Email"  className="p-3"/>
+                <button className="btn primary">Submit</button>
             </form>
             </div>
         </div>
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,5 +1,35 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase.config";
+import { useState } from "react";
 
 const Register = () => {
+  const [registerError, setRegisterError] = useState('')
+  const [success, setSuccess] = useState('')
+
+  const handleLogin = e=>{
+    e.preventDefault()
+    const email = e.target.email.value;
+    const passowrd = e.target.password.value;
+
+    if(passowrd.length < 6){
+      setRegisterError('Password should be at least 6 characters or longer')
+      return
+    }
+
+    setRegisterError('')
+    setSuccess('')
+
+    createUserWithEmailAndPassword(auth, email, passowrd)
+    .then(result => {
+        console.log(result.user);
+        setSuccess('Successfully Login')
+    })
+    .catch(error =>{
+        console.log(error.message);
+        setRegisterError(error.message)
+    })
+}
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -8,19 +38,19 @@ const Register = () => {
       <h1 className="text-5xl font-bold">Login now!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-5">
+      <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" name="password" placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -29,6 +59,12 @@ const Register = () => {
           <button className="btn btn-primary">Login</button>
         </div>
       </form>
+      {
+        registerError && <p className="text-red-500">{registerError}</p>
+      }
+      {
+        success && <p className="text-green-500">{success}</p>
+      }
     </div>
   </div>
 </div>
