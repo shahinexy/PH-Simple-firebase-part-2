c52a79b4f1dd17689e245fbff328f9eea081e709
shahinexy <shahinexy@gmail.com>
2024-03-31 15:08:24 +0600

    login fild added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -2,11 +2,16 @@
 const Login = () => {
     return (
         <div>
-            <h2 className='text-3xl'>Login Now</h2>
-            <form>
-                <input type="text" name="name" id="" placeholder="Enter Name" />
-                <input type="email" name="email" id="" placeholder="Enter Email" />
+            <div className="w-1/2 mx-auto bg-gray-300 p-10 my-5">
+            <h2 className='text-3xl my-5'>Login Now</h2>
+            <form className="space-y-5">
+                <input type="text" name="name" id="" placeholder="Enter Name"  className="p-3"/>
+                <br />
+                <input type="email" name="email" id="" placeholder="Enter Email"  className="p-3"/>
+                <br />
+                <input type="passowrd" name="password" id="" placeholder="Enter Email"  className="p-3"/>
             </form>
+            </div>
         </div>
     );
 };
